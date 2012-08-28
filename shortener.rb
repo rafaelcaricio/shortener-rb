799ad7fcc7ff0a1a7b1e457476cb1bf11b665ef2
Rafael Caricio <rafael.jacinto@gmail.com>
2012-08-28 18:56:13 -0300

    Models within a module.

--- shortener.rb
+++ shortener.rb
@@ -7,18 +7,21 @@ require 'sinatra/activerecord'
 
 set :database, ENV['HEROKU_POSTGRESQL_ROSE'] || "sqlite3://database.db"
 
+module Models
 
-class ShortenedUrl < ActiveRecord::Base
-  validates_format_of :original_url, with: /^https?:\/\/.*$/
-  validates_uniqueness_of :original_url
-  validates_presence_of :original_url
-  has_many :access_to_url, :class_name => 'AccessToUrl', :dependent => :destroy, :foreign_key => 'shortened_url_id'
-end
+  class ShortenedUrl < ActiveRecord::Base
+    validates_format_of :original_url, with: /^https?:\/\/.*$/
+    validates_uniqueness_of :original_url
+    validates_presence_of :original_url
+    has_many :access_to_url, :class_name => 'AccessToUrl', :dependent => :destroy, :foreign_key => 'shortened_url_id'
+  end
 
 
-class AccessToUrl < ActiveRecord::Base
-  validates_presence_of :browser_name
-  belongs_to :shortened_url, :class_name => 'ShortenedUrl'
+  class AccessToUrl < ActiveRecord::Base
+    validates_presence_of :browser_name
+    belongs_to :shortened_url, :class_name => 'ShortenedUrl'
+  end
+
 end
 
 
@@ -32,20 +35,20 @@ module UrlShortener
 
     post '/' do
       not_found unless params[:url]
-      shortened_url = ShortenedUrl.find_or_create_by_original_url params[:url]
+      shortened_url = Models::ShortenedUrl.find_or_create_by_original_url params[:url]
       redirect to("http://#{request.host}/#{shortened_url.id.to_s(36)}+")
     end
 
     get %r{^/(\w+)/?$} do |sequence|
-      shortened_url = ShortenedUrl.find_by_id(sequence.to_i(36))
+      shortened_url = Models::ShortenedUrl.find_by_id(sequence.to_i(36))
       not_found unless shortened_url
       browser_name = 'other' unless request.user_agent
-      AccessToUrl.create(browser_name: browser_name, shortened_url: shortened_url)
+      Models::AccessToUrl.create(browser_name: browser_name, shortened_url: shortened_url)
       redirect to(shortened_url.original_url)
     end
 
     get %r{^/(\w+)\+/?$} do |sequence|
-      @shortened_url = ShortenedUrl.find_by_id(sequence.to_i(36))
+      @shortened_url = Models::ShortenedUrl.find_by_id(sequence.to_i(36))
       not_found unless @shortened_url
       @shorten_url = "http://#{request.host}/#{@shortened_url.id.to_s(36)}"
       erb :analytics
--- spec/shortener_spec.rb
+++ spec/shortener_spec.rb
@@ -21,18 +21,18 @@ describe 'The url shortener application' do
   end
 
   it 'should be possible to be redirected when access an shortened url' do
-    ShortenedUrl.create original_url: "http://caricio.com"
+    Models::ShortenedUrl.create original_url: "http://caricio.com"
 
     get '/1'
     last_response.status.should == 302
     last_response['Location'].should == 'http://caricio.com'
 
-    url = ShortenedUrl.find_by_original_url "http://caricio.com"
+    url = Models::ShortenedUrl.find_by_original_url "http://caricio.com"
     url.access_to_url.size.should == 1
   end
 
   it 'should be possible to see analytivcs for the shortened url I just made' do
-    ShortenedUrl.create original_url: "http://caricio.com"
+    Models::ShortenedUrl.create original_url: "http://caricio.com"
 
     get '/1+'
     last_response.status.should == 200
@@ -52,17 +52,17 @@ end
 describe 'With my models' do
 
   it 'I can create a url' do
-    ShortenedUrl.create original_url: "http://caricio.com"
-    from_db = ShortenedUrl.find_by_original_url "http://caricio.com"
+    Models::ShortenedUrl.create original_url: "http://caricio.com"
+    from_db = Models::ShortenedUrl.find_by_original_url "http://caricio.com"
     from_db.original_url.should == "http://caricio.com"
   end
 
   it 'I can create an access to an url' do
-    url = ShortenedUrl.create original_url: "http://caricio.com"
-    AccessToUrl.create browser_name: 'chrome', shortened_url: url
-    AccessToUrl.create browser_name: 'firefox', shortened_url: url
+    url = Models::ShortenedUrl.create original_url: "http://caricio.com"
+    Models::AccessToUrl.create browser_name: 'chrome', shortened_url: url
+    Models::AccessToUrl.create browser_name: 'firefox', shortened_url: url
 
-    url = ShortenedUrl.find_by_original_url "http://caricio.com"
+    url = Models::ShortenedUrl.find_by_original_url "http://caricio.com"
     url.access_to_url.size.should == 2
     url.access_to_url.order('created_at').first.browser_name.should == 'chrome'
   end
